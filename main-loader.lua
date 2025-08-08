0eb4f36f0d005c993adb79fecd272d3d8c375a25
Founder_on <galinha.chata.muito.chata@gmail.com>
2025-08-07 23:05:35 -0300

    Update main-loader.lua

--- main-loader.lua
+++ main-loader.lua
@@ -1,8 +1,11 @@
-local placeId = game.PlaceId
+local obfuscatedIDs = {
+    [109983668079237] = true,
+    [96342491571673] = true
+}
 
 if placeId == 79546208627805 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BananaCatbeststaff/GatoHubOnTop/refs/heads/main/99-nights.lua"))()
-elseif placeId == 109983668079237 or 96342491571673 then
+elseif obfuscatedIDs[placeId] then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BananaCatbeststaff/GatoHubOnTop/refs/heads/main/obfuscated.lua"))()
 else
     warn("Nenhum script definido para este PlaceId:", placeId)
